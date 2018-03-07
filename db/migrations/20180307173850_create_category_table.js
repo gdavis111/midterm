6fa988b7ae0742f160f0717855748ebfab8b5321
hlowso <h.canderleigh@gmail.com>
2018-03-07 18:26:51 +0000

    initial database tables set up

--- db/migrations/20180307173850_create_category_table.js
+++ db/migrations/20180307173850_create_category_table.js
@@ -1,10 +1,11 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
+  return knex.schema.createTable('categories', function (table) {
     table.increments('id');
     table.string('name');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTable('categories');
 };
+
